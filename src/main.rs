3b27d04bfda821e29c2c2c73300ad69e68f08e46
Tung Do <134229448+tungdo8842@users.noreply.github.com>
2026-01-22 00:02:00 -0800

    Add initial command line argument parsing

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,24 @@
+use std::env;
+use std::fs;
+
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<String> = env::args().collect();
+
+    // will change as more functionality is added
+    if args.len() != 3 {
+        println!("Usage: minigrep <query> <file_path>");
+        return;
+    }
+
+    // initial implementation of argument parsing, will refactor
+    // let query = &args[1];
+    let file_path = &args[2];
+
+    // print!("Searching for {}", query);
+    // println!(" in file {}", file_path);
+
+    let content = fs::read_to_string(file_path)
+        .expect("File not readable");
+
+    println!("File content: \n\n{}", content);
 }
